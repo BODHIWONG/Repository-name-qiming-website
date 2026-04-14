4f1f8da98f63038d054a7419e16a5f0825e6c474
BODHIWONG <57991900+BODHIWONG@users.noreply.github.com>
2026-04-14 13:33:09 +0800

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,3 @@
-/*
-  Home - QIMING FENG SHUI
-*/
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import ServicesSection from "@/components/ServicesSection";
