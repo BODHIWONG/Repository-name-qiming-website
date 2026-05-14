d3c28bc0e345f8322ce2e7d62d5f47362ce8d17b
BODHIWONG <57991900+BODHIWONG@users.noreply.github.com>
2026-05-14 13:00:41 +0800

    Add journal section to qmmind homepage

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import HeroSection from "@/components/HeroSection";
 import ServicesSection from "@/components/ServicesSection";
 import MasterSection from "@/components/MasterSection";
 import VideoSection from "@/components/VideoSection";
+import JournalSection from "@/components/JournalSection";
 import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
 import FloatingActions from "@/components/FloatingActions";
@@ -15,6 +16,7 @@ export default function Home() {
       <ServicesSection />
       <MasterSection />
       <VideoSection />
+      <JournalSection />
       <ContactSection />
       <Footer />
       <FloatingActions />
